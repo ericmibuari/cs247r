af3ea3fd09c107cf9c7c9144a356a5751f8c83b4
ericmibuari <36046699+ericmibuari@users.noreply.github.com>
2018-11-11 01:41:47 -0500

    Update posit_converter.py

--- posit_converter.py
+++ posit_converter.py
@@ -1,46 +1,44 @@
-# Code to convert back and forth between fixed point numbers and posits
+# Code to convert back and forth between fixed point numbers, float point numbers and posits
 
 import torch
 import math
 
-def convert_int_to_bin(fixed_num, useed):
-    
-    #print("\n-----------------")
-    #print("fixed_num",fixed_num, "useed", useed)
+num_bits_fixed_point = 8
+num_frac_bits = 5
+
+def reverse_string(input_string):
+    new_string = ""
+    for i in range(len(input_string)):
+        index = len(input_string) -1 - i
+        val = input_string[index]
+        new_string += str(val)
+    return new_string
+
+def convert_int_to_bin(float_num):
+    bin_div = 2
     
     bit_string = []
-    result = fixed_num
+    result = float_num
     remainder = 0
     
     result = int(result)
-    useed = int(useed)
+    bin_div = int(bin_div) #TODO: might be unnecessary
     #while(result >= useed):
     while(result > 0):
         #print("result",result, "useed", useed)
-        result_div = int(result/useed)
+        result_div = int(result/bin_div)
         #print("result", result, "result_div", result_div, "result_div * useed", result_div * useed)
-        remainder = result - (result_div * useed )
+        remainder = result - (result_div * bin_div )
         #print("appending remainder: ", remainder)
         result = result_div
         bit_string.append(remainder)
-  
-    
-    #TODO: improve/simplify string reversal
-    #new_string = bit_string[-1::-1]
-    #"""
-    new_string = ""
-    for i in range(len(bit_string)):
-        index = len(bit_string) -1 - i
-        val = bit_string[index]
-        new_string += str(val)
-    #"""
-    #print("-----------------\n")
-    return new_string, str(remainder)
+    new_string = reverse_string(bit_string)
+    return new_string
 
-def compute_useed_power(fixed_num, useed,es):
+def compute_useed_power(float_num, useed,es):
 
     bit_string = []
-    result = fixed_num
+    result = float_num
 
     remainder = 0
     
@@ -54,20 +52,32 @@ def compute_useed_power(fixed_num, useed,es):
         #print("remainder", remainder)
         result = result_div
         if(remainder > 1):
-            remainder, frc = convert_int_to_bin(remainder, 2)
+            remainder = convert_int_to_bin(remainder)
         bit_string.append(remainder)
-    #bit_string.append(remainder)
+    new_string = reverse_string(bit_string)
 
-    #TODO: improve/simplify string reversal
-    #new_string = bit_string[-1::-1]
-    #"""
-    for i in range(len(bit_string)):
-        index = len(bit_string) -1 - i
-        val = bit_string[index]
-        new_string += str(val)
-    #"""
     return new_string[0:es], str(remainder)
 
+def convert_fractional_decimal_to_bin(fractional_decimal, num_frac_bits):
+    result = fractional_decimal
+    decimals = ""
+    bits_available = num_frac_bits
+    
+    while bits_available > 0:
+
+        result = result * 2
+        if(result < 1):
+            decimals+= "0"
+        else:
+            decimals+= "1"
+            result = result - 1
+        
+        bits_available -= 1
+    
+    #new_string =  decimals[-1::-1]
+    return decimals
+
+"""
 def convert_fractional_part(fp_value, useed):
     result = fp_value
     decimals = ""
@@ -75,7 +85,7 @@ def convert_fractional_part(fp_value, useed):
         result = result * 2
         decimals+= "0"
     
-    #TODO: convert this number of decimals from fixed(3) to flexible
+    #TODO: convert this number of decimals from constant(3) to flexible
     decimals+= "1"
     fractional = result - 1
     for i in range(3):
@@ -86,6 +96,7 @@ def convert_fractional_part(fp_value, useed):
 
     new_string =  decimals[-1::-1]
     return new_string,new_string
+"""
 
 def get_useed_exponent(num_regime_bits, regime_sign):
     regime_bits = ""
@@ -110,11 +121,11 @@ def get_new_exponent(integral):
     return counter-1
 
 #Reference: posit paper section 4 "Converting values into posits"
-def convert_fixed_to_posit(fp_value, nbits, es):
+def convert_float_to_posit(fp_value, nbits, es):
     #nbits = wordsize #es - maximum size of exponent field
     
     exponent_bits, regime_bits, fraction_bits = (0,0,0)
-    #TODO: Determine the above parameters dynamically from the value of the fixed num provided
+    #TODO: Determine the above parameters dynamically from the value of the float num provided
 
     sign_bit = "0"
     if (fp_value <= 0):
@@ -142,27 +153,21 @@ def convert_fixed_to_posit(fp_value, nbits, es):
         useed_power =  get_useed_exponent(3, 1)
 
     integral = fp_value/(useed **(-3))
-    #print("integral", integral)
+    print("integral", integral)
     new_exponent = get_new_exponent(integral)
 
     #print("new_exponent: ", new_exponent)
     fraction_bits = ""
 
     #TODO: The 2 param is redundant, change function signature
-    new_exponent_bits, useless_frac = convert_int_to_bin(new_exponent,2)
+    new_exponent_bits = convert_int_to_bin(new_exponent)
     
     integral_remainder = integral - (2**new_exponent)
     
     fractional_integer = (useed/(2** new_exponent)) * integral_remainder
-    #print("fractional_integer", fractional_integer)
-    fraction_bits, uselessfrac = convert_int_to_bin(fractional_integer,2)
+    print("fractional_integer", fractional_integer)
+    fraction_bits = convert_int_to_bin(fractional_integer)
     #print("fraction_bits", fraction_bits)
-
-    #other_power, fraction = convert_fractional_part(fp_value, useed)
-
-    #exponent, bit_fraction = convert_int_to_bin(fp_value, 2)
-    #fraction_bits, fracre = convert_int_to_bin(fraction, 2)
-    
         
     #print("sign_bit: ", sign_bit, "useed_power: ",useed_power, "exponent: ", new_exponent_bits, "fraction_bits: ",fraction_bits)
         
@@ -186,7 +191,7 @@ def all_zero(posit_str):
 
 
 #Reference: posit paper section 2
-def convert_posit_to_fixed(es, posit_string):
+def convert_posit_to_float(es, posit_string):
     
     if all_zero(posit_string):
         print("expression_val", 0)
@@ -218,25 +223,110 @@ def convert_posit_to_fixed(es, posit_string):
 
     return expression_val
 
+def convert_fixed_point_to_float(fixed_point_num):
 
-test_numbers = [3.553926944732666e-06, 3.553926946732666e-06, 3.553926946732666e-04]
+    #num_bits_fixed_point
+    #print("fixed_point_num: ", fixed_point_num)
+    
+    fixed_point_num_string = str(fixed_point_num)
+    sign = int(fixed_point_num_string[0])
+    #print("sign: ", sign)
+    fixed_point_num = fixed_point_num_string[1:]
+    #print("fixed_point_num: ", fixed_point_num)
+    radix_index = fixed_point_num.index('.')
+    #print("radix_index: ", radix_index)
+    integral_part = fixed_point_num[0:radix_index]
+    #print("integral_part: ", integral_part)
+    fractional_part = fixed_point_num[radix_index+1:]
+    #print("fractional_part: ", fractional_part)
+    
+    integral_part_reversed =  reverse_string(integral_part)
+    
+    integral_sum = 0
+    for i in range(len(integral_part_reversed)):
+        num_read = int(integral_part_reversed[i])
+        #print("num_read: ", num_read)
+        if num_read == 1:
+            #print("adding: ", 2**i )
+            integral_sum += 2**i
+    #print("integral_sum: ", integral_sum)
 
-for i in range(len(test_numbers)):
-    print("\n-------------------------")
-    num = test_numbers[i]
-    print("converting ", num, " to posit" )
-    posit_value = convert_fixed_to_posit( num, 16, 3)
-    print("onbtained posit value: ", posit_value )
-    print("converting posit ",posit_value, "back to fixed " )
-    fixed_point_value = convert_posit_to_fixed(3, posit_value)
-    print("onbtained fixed point value: ", fixed_point_value )
-    print("diff: ", num - fixed_point_value )
-    print("-------------------------\n")
+    fractional_sum = 0
+    for j in range(len(fractional_part)):
+        num_read = int(fractional_part[j])
+        #print("num_read: ", num_read)
+        if num_read == 1:
+            #print("adding: ", float(1)/2**(j+1) )
+            fractional_sum += float(1)/2**(j+1)
+    #print("fractional_sum: ", fractional_sum)
+
+    total_float = integral_sum + fractional_sum
+
+    if sign == 1:
+        total_float = total_float * -1
+
+    return total_float
+    
+def convert_float_to_fixed_point(float_num):
+    fp_string = ""
+    abso_val = float_num
+    if float_num < 0:
+        fp_string += "1"
+        abso_val = abso_val * -1
+    else:
+        fp_string += "0"
+
+    fractional_part = 0
+    integral_part = 0
+    if abso_val < 1:
+        fractional_part = abso_val
+    else:
+        integral_part = int(abso_val)
+        fractional_part = abso_val - integral_part
+
+    int_bin_string = convert_int_to_bin(integral_part)
+    frac_bin_string = convert_fractional_decimal_to_bin(fractional_part, num_frac_bits)
+
+    return fp_string + int_bin_string +"." + frac_bin_string
 
 
+def test_conversions2():
+    
+    test_numbers = [3.553926946732666e-06]
+    num = 3.553926946732666e-06
+    for i in range(15):
+        posit_value = convert_float_to_posit( num, 16, i)
+        print("onbtained posit value: ", posit_value,"\n" )
 
-#print(convert_posit_to_fixed(3, "0000110111011101"))
-#print(convert_fixed_to_posit( 3.553926944732666e-06, 16, 3))
+
+def test_conversions():
+
+    #test_numbers = [3.553926944732666e-06, 3.553926946732666e-06, 3.553926946732666e-04]
+    test_numbers = [3.553926944732666e-06, 3.553926946732666e-06]
+
+    for i in range(len(test_numbers)):
+        print("\n-------------------------")
+        num = test_numbers[i]
+        print("converting float ", num, " to posit" )
+        posit_value = convert_float_to_posit( num, 16, 3)
+        print("onbtained posit value: ", posit_value )
+        print("converting posit ",posit_value, "back to float " )
+        float_point_value = convert_posit_to_float(3, posit_value)
+        print("onbtained float point value: ", float_point_value )
+        print("diff: ", num - float_point_value )
+        print("-------------------------\n")
+
+#test_conversions2()
+#print(reverse_string("110111111001110"))
+
+#print(convert_fixed_point_to_float("11001.0111"))
+print(convert_float_to_fixed_point(-9.124343757546456))
+
+print(convert_fixed_point_to_float("11001.00011"))
+
+
+#print(convert_posit_to_float(3, "0000110111011101"))
+#print(convert_float_to_posit( 3.553926944732666e-06, 16, 3))
 
 
 #TODO: Integrate with quantization after
@@ -276,13 +366,3 @@ g_int = 2
 g_float = 3
 
 #print(quantize_(num_full, g_int, g_float))
-
-
-
-
-
-
-
-
-
-
